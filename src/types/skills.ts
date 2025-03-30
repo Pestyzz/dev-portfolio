00c69e81e8caffb8472c1209152f265eeffaeb4f
Bastian <ba.niripil@duocuc.cl>
2025-03-29 23:35:22 -0300

    modify skills types

--- src/types/skills.ts
+++ src/types/skills.ts
@@ -1,4 +1,5 @@
 type SkillID = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "13" | "14" | "15" | "16";
+
 type SkillName = "Angular" | "Astro" | "Ionic" | "Django" | "Csharp" | "Java" | "Python" | "Typescript" | "Javascript" | "TailwindCSS" | "Bootstrap" | "CSS" | "HTML5" | "Firebase" | "PostgreSQL" | "Oracle SQL" | "Git";
 
 export interface Skill {
