e511ff921900b18fd3c011ad83047e3e34a23f81
Bastian <ba.niripil@duocuc.cl>
2025-03-29 23:17:15 -0300

    add skill types and const

---
+++ src/consts/skills.ts
@@ -0,0 +1,101 @@
+import type { Skill } from "../types/skills";
+
+import AngularIcon from "../assets/icons/Angular.svg";
+import AstroIcon from "../assets/icons/Astro.svg";
+import IonicIcon from "../assets/icons/Ionic.svg";
+import DjangoIcon from "../assets/icons/Django.svg";
+import CsharpIcon from "../assets/icons/Csharp.svg";
+import JavaIcon from "../assets/icons/Java.svg";
+import PythonIcon from "../assets/icons/Python.svg";
+import TypescriptIcon from "../assets/icons/Typescript.svg";
+import JavascriptIcon from "../assets/icons/Javascript.svg";
+import TailwindIcon from "../assets/icons/TailwindCSS.svg";
+import BootstrapIcon from "../assets/icons/Bootstrap.svg";
+import CSSIcon from "../assets/icons/CSS.svg";
+import HTML5Icon from "../assets/icons/HTML5.svg";
+import FirebaseIcon from "../assets/icons/Firebase.svg";
+import PostgreSQLIcon from "../assets/icons/PostgreSQL.svg";
+import OracleSQLIcon from "../assets/icons/OracleSQL.svg";
+
+export const SKILLS: Skill[] = [
+    {
+        id: "1",
+        logo: AngularIcon,
+        name: "Angular"
+    },
+    {
+        id: "2",
+        logo: AstroIcon,
+        name: "Astro"
+    },
+    {
+        id: "3",
+        logo: IonicIcon,
+        name: "Ionic"
+    },
+    {
+        id: "4",
+        logo: DjangoIcon,
+        name: "Django"
+    },
+    {
+        id: "5",
+        logo: CsharpIcon,
+        name: "Csharp"
+    },
+    {
+        id: "6",
+        logo: JavaIcon,
+        name: "Java"
+    },
+    {
+        id: "7",
+        logo: PythonIcon,
+        name: "Python"
+    },
+    {
+        id: "8",
+        logo: TypescriptIcon,
+        name: "Typescript"
+    },
+    {
+        id: "9",
+        logo: JavascriptIcon,
+        name: "Javascript"
+    },
+    {
+        id: "10",
+        logo: TailwindIcon,
+        name: "TailwindCSS"
+    },
+    {
+        id: "11",
+        logo: BootstrapIcon,
+        name: "Bootstrap"
+    },
+    {
+        id: "12",
+        logo: CSSIcon,
+        name: "CSS"
+    },
+    {
+        id: "13",
+        logo: HTML5Icon,
+        name: "HTML5"
+    },
+    {
+        id: "14",
+        logo: FirebaseIcon,
+        name: "Firebase"
+    },
+    {
+        id: "15",
+        logo: PostgreSQLIcon,
+        name: "PostgreSQL"
+    },
+    {
+        id: "16",
+        logo: OracleSQLIcon,
+        name: "Oracle SQL"
+    }
+] 
